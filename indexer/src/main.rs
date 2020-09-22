b049e8824260aebe41ca1673d5ab5fa300a7f39a
Nate Geier <nate@mintbase.io>
2020-09-22 15:40:01 +0200

    test 1

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -60,33 +60,33 @@ fn main() {
     .with_writer(std::io::stderr)
     .init();
 
-  let opts: Opts = Opts::parse();
+  // let opts: Opts = Opts::parse();
 
-  let home_dir = opts
-    .home_dir
-    .unwrap_or(std::path::PathBuf::from(near_indexer::get_default_home()));
+  // let home_dir = opts
+  //   .home_dir
+  //   .unwrap_or(std::path::PathBuf::from(near_indexer::get_default_home()));
 
-  match opts.subcmd {
-    SubCommand::Run => {
-      let indexer_config = near_indexer::IndexerConfig {
-        home_dir,
-        sync_mode: near_indexer::SyncModeEnum::FromInterruption,
-      };
-      let indexer = near_indexer::Indexer::new(indexer_config);
-      let stream = indexer.streamer();
-      actix::spawn(listen_blocks(stream));
-      indexer.start();
-    }
-    SubCommand::Init(config) => near_indexer::init_configs(
-      &home_dir,
-      config.chain_id.as_ref().map(AsRef::as_ref),
-      config.account_id.as_ref().map(AsRef::as_ref),
-      config.test_seed.as_ref().map(AsRef::as_ref),
-      config.num_shards,
-      config.fast,
-      config.genesis.as_ref().map(AsRef::as_ref),
-      config.download,
-      config.download_genesis_url.as_ref().map(AsRef::as_ref),
-    ),
-  }
+  // match opts.subcmd {
+  //   SubCommand::Run => {
+  //     let indexer_config = near_indexer::IndexerConfig {
+  //       home_dir,
+  //       sync_mode: near_indexer::SyncModeEnum::FromInterruption,
+  //     };
+  //     let indexer = near_indexer::Indexer::new(indexer_config);
+  //     let stream = indexer.streamer();
+  //     actix::spawn(listen_blocks(stream));
+  //     indexer.start();
+  //   }
+  //   SubCommand::Init(config) => near_indexer::init_configs(
+  //     &home_dir,
+  //     config.chain_id.as_ref().map(AsRef::as_ref),
+  //     config.account_id.as_ref().map(AsRef::as_ref),
+  //     config.test_seed.as_ref().map(AsRef::as_ref),
+  //     config.num_shards,
+  //     config.fast,
+  //     config.genesis.as_ref().map(AsRef::as_ref),
+  //     config.download,
+  //     config.download_genesis_url.as_ref().map(AsRef::as_ref),
+  //   ),
+  // }
 }
